9d7603ae205b5e1d237f0322d992a3516b8e92d8
David Yee <github@mail.davidjyee.dev>
2021-05-28 12:18:52 -0400

    Implement loading

--- atm_client.lua
+++ atm_client.lua
@@ -28,6 +28,49 @@ RegisterNUICallback('focus', function(data, cb)
 	cb({})
 end)
 
+RegisterNUICallback('access/add', function(data, cb)
+	local accessId = data.id or -1
+
+	if(accessId >= -1) then
+		TriggerEvent("chat:addMessage", {
+			args = { 'Attempting to add access with id ' .. accessId .. ' with user ' .. data.userId .. ' for account ' .. data.accountId .. ' of access level ' .. data.accessLevel }
+		})
+	end
+
+	cb({
+		success = 'false'
+	})
+end)
+
+RegisterNUICallback('access/edit', function(data, cb)
+	local accessId = data.id or -1
+	local accessLevel = data.level or 0
+
+	if(accessId >= -1) then
+		TriggerEvent("chat:addMessage", {
+			args = { 'Attempting to edit access with id ' .. accessId .. ' to level ' .. accessLevel }
+		})
+	end
+
+	cb({
+		success = 'false'
+	})
+end)
+
+RegisterNUICallback('access/remove', function(data, cb)
+	local accessId = data.id or -1
+
+	if(accessId >= 0) then
+		TriggerEvent("chat:addMessage", {
+			args = { 'Attempting to remove access with id ' .. accessId }
+		})
+	end
+
+	cb({
+		success = 'false'
+	})
+end)
+
 RegisterNUICallback('transaction/commit', function(data, cb)
 	local type = data.type
 	local amount = data.amount
@@ -60,5 +103,89 @@ RegisterNUICallback('transaction/commit', function(data, cb)
 	-- Return that it was a success
 	cb({
 		success = 'true'
+	})
+end)
+
+RegisterNUICallback('retrieve', function(data, cb)
+	-- Retrieve user information
+	local user = {
+		id = 816,
+		name = 'Akihiro Sakamoto',
+		cash = 500
+	}
+
+	-- Retrieve transactions
+	local transactions = {
+		{
+			id = 0,
+			type = 'DEPOSIT',
+			origin = -1,
+			destination = 100,
+			initiator = 816,
+			time = '2016-05-25T09:08:34.123Z',
+			amount = 100,
+			note = 'Cash Deposit'
+		},
+		{
+			id = 1,
+			type = 'WITHDRAWAL',
+			origin = 100,
+			destination = -1,
+			initiator = 816,
+			time = '2016-05-25T09:08:34.123Z',
+			amount = 100,
+			note = 'Cash Withdrawal'
+		},
+		{
+			id = 2,
+			type = 'TRANSFER',
+			origin = 100,
+			destination = 200,
+			initiator = 816,
+			time = '2016-05-25T09:08:34.123Z',
+			amount = 100,
+			note = 'Test Transfer'
+		},
+	}
+
+	-- Retrieve access information for the given user
+	local access = {
+		{
+			id = 0,
+			userId = 816,
+			accountId = 123456,
+			accessLevel = 1
+		},
+		{
+			id = 1,
+			userId = 816,
+			accountId = 987654,
+			accessLevel = 1
+		}
+	}
+
+	-- Retrieve account information of related accounts
+	local accounts = {
+		{
+			id = 123456,
+			name = 'Personal',
+			type = 'Personal',
+			routing = 100,
+			balance = 5000
+		},
+		{
+			id = 987654,
+			name = 'Pillbox',
+			type = 'Business',
+			routing = 200,
+			balance = 10000
+		}
+	}
+
+	cb({
+		user = user,
+		transactions = transactions,
+		access = access,
+		accounts = accounts
 	})
 end)
